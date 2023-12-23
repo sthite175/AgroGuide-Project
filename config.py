3ee54dd870eef9059fd93232944f2120fe0fe6bf
sthite175 <sthite175@gmail.com>
2023-12-23 09:23:13 +0530

    File Added

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+model_path = os.path.join('artifact','new_rf_model.pickle')
+features_path = os.path.join('artifact','features_data.json')
+labels_path = os.path.join('artifact','class_labels.json')
+
+PORT = 8080
+HOST = '0.0.0.0'
+
